e1157fb6544afd23c737433fb21dfa585b78718d
Denys Tsybko <72654542+edexmusic@users.noreply.github.com>
2021-05-10 15:08:40 +0300

    Add files via upload

---
+++ UnitTestLab2.4/UnitTestLab2.4.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab2.4/Matrix.cpp"
+#include "../lab2.4/Matrix.h"
+#include "../lab2.4/Row.cpp"
+#include "../lab2.4/Row.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest24
+{
+	TEST_CLASS(UnitTest24)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			Matrix A(4), B(4);
+			Assert::AreEqual(A == B, true);
+		}
+	};
+}
